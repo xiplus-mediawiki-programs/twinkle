cb7ae0626d6c85b84782da7dbf8feb756dffc935
Amory Meltzer <Amorymeltzer@gmail.com>
2019-09-26 06:17:02 -0400

    Convert mw.RegExp.escape to mw.util.escapeRegExp as the former is being deprecated
    From https://github.com/azatoth/twinkle/commit/6dbd6f0abd4d7220631c73518a825520d9318bc2

--- modules/friendlytag.js
+++ modules/friendlytag.js
@@ -90,7 +90,7 @@ Twinkle.tag.callback = function friendlytagCallback() {
 						allCheckboxDivs.hide();
 						allHeaders.hide();
 						var searchString = this.value;
-						var searchRegex = new RegExp(mw.RegExp.escape(searchString), 'i');
+						var searchRegex = new RegExp(mw.util.escapeRegExp(searchString), 'i');
 
 						$form.find('label').each(function() {
 							var label_text = this.textContent;
