77417de444d7ed1b9172bb62e94c0b0455a54328
DannyS712 <46829944+DannyS712@users.noreply.github.com>
2020-01-23 16:03:15 -0800

    Hide space for portel if unused
    https://github.com/wikimedia-gadgets/twinkle/commit/5cff2a80cb37a317fcef48af683a3bd45e63254e
    
    Co-Authored-By: Amory Meltzer <Amorymeltzer@gmail.com>

--- twinkle.js
+++ twinkle.js
@@ -504,6 +504,11 @@ Twinkle.load = function () {
 		mw.util.addCSS('.morebits-dialog-content, .morebits-dialog-footerlinks { font-size: 100% !important; } ' +
 			'.morebits-dialog input, .morebits-dialog select, .morebits-dialog-content button { font-size: inherit !important; }');
 	}
+
+	// Hide the lingering space if the TW menu is empty
+	if (mw.config.get('skin') === 'vector' && Twinkle.getPref('portletType') === 'menu' && $('#p-twinkle').length === 0) {
+		$('#p-cactions').css('margin-right', 'initial');
+	}
 };
 
 /** Twinkle-specific utility functions shared by multiple modules */
