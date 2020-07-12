4d8268484bd922722b6c7ca5540fd78a69ecf638
xiplus <huangxuanyuxiplus@gmail.com>
2020-07-12 09:32:55 +0800

    unlink: Update summray

--- modules/twinkleunlink.js
+++ modules/twinkleunlink.js
@@ -298,7 +298,7 @@ Twinkle.unlink.callbacks = {
 		}
 
 		pageobj.setPageText(text);
-		pageobj.setEditSummary(summaryText + ' "' + Morebits.pageNameNorm + '": ' + params.reason + '.' + Twinkle.getPref('summaryAd'));
+		pageobj.setEditSummary(summaryText + wgULS('“', '「') + Morebits.pageNameNorm + wgULS('”：', '」：') + params.reason + Twinkle.getPref('summaryAd'));
 		pageobj.setTags(Twinkle.getPref('revisionTags'));
 		pageobj.setCreateOption('nocreate');
 		pageobj.save(params.unlinker.workerSuccess, params.unlinker.workerFailure);
