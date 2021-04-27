2f49738d0270b890e1a13a95d29dfc806d2f451b
xiplus <huangxuanyuxiplus@gmail.com>
2021-04-27 19:08:12 +0800

    config: Add O7 and enable O7, O8 by default

--- twinkle.js
+++ twinkle.js
@@ -103,8 +103,8 @@ Twinkle.defaultConfig = {
 	markSpeedyPagesAsPatrolled: true,
 
 	// these next two should probably be identical by default
-	notifyUserOnSpeedyDeletionNomination: [ 'db', 'g1', 'g2', 'g3', 'g5', 'g11', 'g12', 'g13', 'g16', 'a1', 'a2', 'a5', 'a6', 'f6', 'r2', 'r3', 'r7' ],
-	welcomeUserOnSpeedyDeletionNotification: [ 'db', 'g1', 'g2', 'g3', 'g5', 'g11', 'g12', 'g13', 'g16', 'a1', 'a2', 'a5', 'a6', 'f6', 'r2', 'r3', 'r7' ],
+	notifyUserOnSpeedyDeletionNomination: [ 'db', 'g1', 'g2', 'g3', 'g5', 'g11', 'g12', 'g13', 'g16', 'a1', 'a2', 'a5', 'a6', 'o7', 'o8', 'f6', 'r2', 'r3', 'r7' ],
+	welcomeUserOnSpeedyDeletionNotification: [ 'db', 'g1', 'g2', 'g3', 'g5', 'g11', 'g12', 'g13', 'g16', 'a1', 'a2', 'a5', 'a6', 'o7', 'o8', 'f6', 'r2', 'r3', 'r7' ],
 	promptForSpeedyDeletionSummary: [],
 	openUserTalkPageOnSpeedyDelete: [ ],
 	deleteTalkPageOnDelete: true,
