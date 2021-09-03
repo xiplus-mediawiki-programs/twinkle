cf1cf412225ea7c7a22a96a65dd3e4a5babd7e4e
xiplus <huangxuanyuxiplus@gmail.com>
2021-09-03 18:08:57 +0800

    speedy: Remove trailing punctuation from summary

--- modules/twinklespeedy.js
+++ modules/twinklespeedy.js
@@ -1303,7 +1303,7 @@ Twinkle.speedy.callbacks = {
 					}
 				});
 				editsummary = editsummary.substr(0, editsummary.length - 1); // remove trailing comma
-				editsummary += '）。';
+				editsummary += '）';
 			} else if (params.normalizeds[0] === 'db') {
 				editsummary = wgULS('请求[[WP:CSD|快速删除]]：', '請求[[WP:CSD|快速刪除]]：') + params.templateParams[0]['1'];
 			} else {
