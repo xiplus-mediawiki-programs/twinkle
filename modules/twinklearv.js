3bed59187c2817b3fbc81c707b40091cb105c9c8
Hamish <hamishz@toolforge.org>
2025-09-11 20:26:51 +0800

    arv: prompt for confirmation before submitting 0-edit account to uaa

--- modules/twinklearv.js
+++ modules/twinklearv.js
@@ -769,6 +769,21 @@ Twinkle.arv.callback.evaluate = function(e) {
 			var uaaPage = new Morebits.wiki.Page('Wikipedia:管理员布告板/不当用户名', conv({ hans: '处理UAA请求', hant: '處理UAA請求' }));
 			uaaPage.setFollowRedirect(true);
 
+			// Check if the user is 0 contribs
+			var noContribs;
+			new mw.Api().get({
+				action: 'query',
+				list: 'users',
+				ususers: uid,
+				usprop: 'editcount',
+				format: 'json'
+			}).then(function(data) {
+				noContribs = Object.keys(data.query.users)[0] === '-1' ? null : data.query.users[Object.keys(data.query.users)[0]].editcount;
+			});
+			if (!noContribs && !confirm(conv({ hans: '用户没有任何编辑。\n根据方针规定，除确认为傀儡或多次遭受骚扰外，不应报告。\n是否继续？', hant: '使用者沒有任何編輯。\n根據方針規定，除確認為傀儡或多次遭受騷擾外，不應報告。\n是否繼續？' }))) {
+				Morebits.Status.error(conv({ hans: '报告用户给管理人员', hant: '報告使用者給管理人員' }), conv({ hans: '用户取消操作', hant: '使用者取消操作' }));
+				return;
+			}
 			uaaPage.load(function() {
 				var text = uaaPage.getPageText();
 
