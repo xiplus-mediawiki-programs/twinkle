47f5313005928663d1d4dda509d7f29acd3e9841
Hamish <hamishz@toolforge.org>
2025-10-05 04:14:47 +0800

    LoadTwinkle.js: dev ver

--- LoadTwinkle.js
+++ LoadTwinkle.js
@@ -1,39 +1,32 @@
 /* eslint-disable no-console */
-/**
- * +-------------------------------------------------------------------------+
- * |                              === 警告 ===                               |
- * |      本工具有一些新穎的修改，雖本人會經過測試，但不能保證不會出錯            |
- * |               強烈建議在使用後複查編輯，特別是那些新功能                    |
- * |                    詳情請見[[User:Xiplus/Twinkle]]                       |
- * +-------------------------------------------------------------------------+
- */
-
 // 複製並修改自 https://zh.wikipedia.org/w/index.php?oldid=45972864 作者為 User:逆襲的天邪鬼
-
+// Adopted from https://zh.wikipedia.org/w/index.php?oldid=60075303 User:Xiplus
 // 載入自己修改的Twinkle
 
-(function() {
-
-var VERSION = '{{subst:#time:Y-m-d H:i:s}}';
-var PREFIX = 'User:Xiplus/Twinkle/';
-var rebuildcache = localStorage.A64Twinkle_version !== VERSION;
+(function () {
+if (mw.config.get('wgUserName') !== 'Hamish') {
+	return;
+}
+var VERSION = 'dev';
+var PREFIX = 'User:Hamish/Twinkle/';
+var rebuildcache = true;
 var tests = [];
-
-var ajax = function(title) {
+var siteBase = mw.config.get('wgServer') + mw.util.wikiScript('index') + '?title=';
+var ajax = function (title) {
 	return $.ajax({
-		url: 'https://zh.wikipedia.org/w/index.php?title=' + title + '&action=raw&ctype=text/javascript',
+		url: siteBase + title + '&action=raw&ctype=text/javascript',
 		dataType: 'text'
 	});
 };
 
-var load = function(p) {
-	var done = function(data) {
-		if (rebuildcache || !localStorage['A64Twinkle_' + p.name]) {
-			localStorage['A64Twinkle_' + p.name] = data;
+var load = function (p) {
+	var done = function (data) {
+		if (rebuildcache || !localStorage['HamishTestTwinkle_' + p.name]) {
+			localStorage['HamishTestTwinkle_' + p.name] = data;
 		}
 	};
-	if (localStorage['A64Twinkle_' + p.name] && !rebuildcache) {
-		return $.Deferred().resolve(localStorage['A64Twinkle_' + p.name]);
+	if (localStorage['HamishTestTwinkle_' + p.name] && !rebuildcache) {
+		return $.Deferred().resolve(localStorage['HamishTestTwinkle_' + p.name]);
 	}
 	if (p.test) {
 		return ajax(PREFIX + p.name).done(done);
@@ -41,14 +34,13 @@ var load = function(p) {
 	return ajax('MediaWiki:Gadget-' + p.name).done(done);
 };
 
-var message = function(text) {
-	console.log('[A64Twinkle]', text);
-	//    $('#simpleSearch input[type="search"]').attr('placeHolder', text);
+var message = function (text) {
+	console.log('[HamishTestTwinkle]', text);
+//    $('#simpleSearch input[type="search"]').attr('placeHolder', text);
 };
 
-tests.push({ name: 'morebits.js', test: true });
 tests.push({ name: 'twinkle.js', test: true });
-tests.push({ name: 'select2.min.js', test: true });
+tests.push({ name: 'morebits.js', test: true });
 tests.push({ name: 'modules/twinklearv.js', test: true });
 tests.push({ name: 'modules/twinklewarn.js', test: true });
 tests.push({ name: 'modules/friendlyshared.js', test: true });
@@ -70,40 +62,48 @@ tests.push({ name: 'modules/twinklespeedy.js', test: true });
 tests.push({ name: 'modules/twinklestub.js', test: true });
 tests.push({ name: 'modules/twinkleunlink.js', test: true });
 tests.push({ name: 'modules/twinklexfd.js', test: true });
+tests.push({ name: 'modules/twinklehandleemail.js', test: true });
 
-mw.loader.using(['mediawiki.user', 'mediawiki.util', 'mediawiki.Title', 'jquery.ui', 'jquery.tipsy', 'jquery.chosen']).done(function() {
-	mw.loader.load('https://zh.wikipedia.org/w/index.php?title=User:Xiplus/Twinkle/morebits.css&action=raw&ctype=text/css', 'text/css');
-	mw.loader.load('https://zh.wikipedia.org/w/index.php?title=User:Xiplus/Twinkle/select2.min.css&action=raw&ctype=text/css', 'text/css');
-	mw.loader.load('https://zh.wikipedia.org/w/index.php?title=User:Xiplus/Twinkle/twinkle.css&action=raw&ctype=text/css', 'text/css');
+// eslint-disable-next-line no-unused-vars
+mw.loader.using(['mediawiki.user', 'mediawiki.util', 'mediawiki.api', 'ext.gadget.morebits', 'jquery.ui', 'mediawiki.language', 'ext.gadget.HanAssist', 'ext.gadget.select2']).then((require) => {
+	if (mw.config.get('wgUserGroups').indexOf('sysop') === -1) {
+		mw.config.get('wgUserGroups').push('sysop');
+	}
+	mw.loader.load(siteBase + 'User:Hamish/Twinkle/twinkle.css&action=raw&ctype=text/css', 'text/css');
+	mw.loader.load(siteBase + 'User:Hamish/Twinkle/twinkle-pagestyles.css&action=raw&ctype=text/css', 'text/css');
+	mw.loader.load(siteBase + 'User:Hamish/Twinkle/morebits.css&action=raw&ctype=text/css', 'text/css');
 
-	var i = 0;
-	// var finished = 0;
+	var i;
+	var finished = 0;
 	var code = [];
 
 	// all
-	message('Loading A64TW...');
+	message('Loading HamishTestTW...');
 	var promises = [];
-	var done = function(x) {
-		return function(data) {
-			// finished++;
-			// message('Loading A64TW... (' + finished + '/' + tests.length + ')');
+	var done = function (x) {
+		return function (data) {
+			finished++;
+			message('Loading HamishTestTW... (' + finished + '/' + tests.length + ')');
 			code[x] = data;
 		};
 	};
 	for (i = 0; i < tests.length; i++) {
 		promises.push(load(tests[i]).done(done(i)));
 	}
-	$.when.apply($, promises).done(function() {
-		localStorage.A64Twinkle_version = VERSION;
+	$.when.apply($, promises).done(function () {
+		localStorage.HamishTestTwinkle_version = VERSION;
 		eval(code.join('\n;\n'));
 		message('Twinkle Done');
-		if ($('#twinkle-config-titlebar').length) {
-			$('#twinkle-config-titlebar').append('--版本：Xiplus ' + localStorage.A64Twinkle_version);
-			$('#twinkle-config-titlebar').append('<button onclick="localStorage.A64Twinkle_version = \'\';location.reload();">清除快取</button>');
+		var configTitle = $('#twinkle-config-titlebar');
+		if (configTitle.length) {
+			configTitle.append('--版本：HamishTest ' + localStorage.HamishTestTwinkle_version);
+			configTitle.append('<button onclick="localStorage.HamishTestTwinkle_version = \'\';location.reload();">清除快取</button>');
 		}
 	});
 });
+})();
 
+/*
 mw.loader.using(['mediawiki.api']).done(function() {
 	var api = new mw.Api();
 
@@ -135,18 +135,8 @@ mw.loader.using(['mediawiki.api']).done(function() {
 			} else {
 				mw.notify('成功為您啟用小工具內的 Twinkle');
 			}
-		}).fail(function(e) {
-			mw.notify('啟用小工具內的 Twinkle 失敗：' + e, { type: 'error' });
 		});
-
-	}).fail(function(e) {
-		if (e === 'nocreate-missing') {
-			console.error('替換Twinkle失敗：找不到您的 common.js');
-		} else {
-			console.error('替換Twinkle失敗：' + e);
-		}
 	});
 
-});
-
 })();
+*/
