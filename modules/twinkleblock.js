bdbadfbe3499ec2a41fc8e6928e710db7a2e99b7
Hamish <hamishz@toolforge.org>
2026-01-27 06:40:47 +0000

    Revert "block: fix undefined variable (#371)"
    This reverts commit 72265dd8fbd5047f7ec88da972ca77d035ebe157.

--- modules/twinkleblock.js
+++ modules/twinkleblock.js
@@ -267,7 +267,7 @@ Twinkle.block.processUserInfo = function twinkleblockProcessUserInfo(data, fn) {
 				return advancedGroups[e.group];
 			})
 			: [];
-		Twinkle.block.isTempAccount = mw.util.isTemporaryUser(blockinfo.user);
+		Twinkle.block.isTempAccount = mw.util.isTemporaryUser(relevantUserName);
 		if (Twinkle.block.isTempAccount && userinfo.registration) {
 			var registrationDate = new Morebits.Date(userinfo.registration);
 			Twinkle.block.tempAccountRegistration = registrationDate;
