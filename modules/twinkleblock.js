68af294d8753946ea6f706c1e97823d9216c4829
Hamish <hamishz@toolforge.org>
2026-06-21 17:06:26 +0800

    block: fix rfdr report signature

--- modules/twinkleblock.js
+++ modules/twinkleblock.js
@@ -2499,7 +2499,8 @@ Twinkle.block.callback.addRevokeReport = function twinkleblockCallbackAddRevokeR
 		'*{{Status|新提案}}\n' +
 		'*需複審或解除之權限：' + Twinkle.block.userAdvancedGroups.join('、') + '\n' +
 		('*理由：使用者已因<nowiki>' + (params.reason || '').replaceAll(/\[\[([^\]]+)\]\]/g, '</nowiki>[[$1]]<nowiki>')).replace(/<nowiki><\/nowiki>/, '') + blockTypeText + '</nowiki>，請覆核考慮是否為之除權。\n' +
-		'*提報人：~~~~\n';
+		'*提報人：';
+	reportContent += '~~' + '~~\n';
 	var sectionStartIndex = sectionMatch.index + sectionMatch[0].length;
 	var remainingText = text.substring(sectionStartIndex);
 	var nextSectionMatch = /^==\s*[^=]+\s*==$/m.exec(remainingText);
